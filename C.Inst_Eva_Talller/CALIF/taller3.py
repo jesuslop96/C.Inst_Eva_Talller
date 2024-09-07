ee9c1681bb618c615d62bd5298f3b9e0e6cf5a1d
jesuslop96 <jesuslopez1296@gmail.com>
2024-09-06 21:30:05 -0500

    Add files via upload

---
+++ C.Inst_Eva_Talller/CALIF/taller3.py
@@ -0,0 +1,25 @@
+calificacion1 = float(input("Ingrese la primera calificación:"))
+calificacion2 = float(input("Ingrese la segunda calificación:"))
+calificacion3 = float(input("Ingrese la tercera calificación:"))
+calificacion4 = float(input("Ingrese la tercera calificación:"))
+Pago = float(input("Ingrese el valor de su matricula:"))
+
+promedio = (calificacion1 + calificacion2 + calificacion3 + calificacion4) / 4
+print("Tu promedio es de:",promedio)
+Excelente = 20
+Bien = 100
+Deficiente = 100
+
+if promedio >=4:
+    print("Excelente obtienes descuento del",Excelente)
+elif promedio >=3 and 3.99:
+    print("Bien cancela el",Bien)
+elif promedio >=0 and 2.99:
+    print("Deficiente cancela el",Deficiente)
+
+if promedio >=4:
+    Desc = Pago * 0.20
+    Totalmat = Pago - Desc
+    print("Tu matricula queda en:",Totalmat)
+else:
+    print("No tiene Beneficio")
---
+++ C.Inst_Eva_Talller/IMC/taller 1.py
@@ -0,0 +1,6 @@
+Peso = float(input("Ingrese su peso en kilogramos:"))
+Altura = float(input("Ingrese su altura en metros:"))
+
+IMC = Peso / Altura**2
+
+print("Su resultado de IMC es:" ,IMC)
